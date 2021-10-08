d5235b664ebd536a93b8a8ffcb1bcf61bb417177
akash <akash.punagin@gmail.com>
2021-10-08 17:06:39 +0530

    seperated components

---
+++ src/components/game.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import Board from "./board";
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{ squares: Array(9).fill(null) }],
+      isXNext: true,
+      stepNumber: 0,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const currentSquareObj = history[history.length - 1];
+    const squares = currentSquareObj.squares.slice();
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.isXNext ? "X" : "O";
+    this.setState({
+      history: history.concat([{ squares }]),
+      stepNumber: history.length,
+      isXNext: !this.state.isXNext,
+    });
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      isXNext: step % 2 === 0 ? "X" : "O",
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const currentSquareObj = history[this.state.stepNumber];
+    const winner = this.calculateWinner(currentSquareObj.squares);
+
+    const moves = history.map((step, index) => {
+      const desc = index ? "Go to Move:" + index : "Go to the Game Start";
+
+      return (
+        <li key={index}>
+          <button onClick={() => this.jumpTo(index)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.isXNext ? "X" : "O");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={currentSquareObj.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
